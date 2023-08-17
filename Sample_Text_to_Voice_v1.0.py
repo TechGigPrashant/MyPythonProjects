8d987a3e59f0b657e068f594132828edfdb22c9f
TechGigPrashant <45738508+TechGigPrashant@users.noreply.github.com>
2023-08-16 23:03:39 -0400

    Adding_Text_To_Voice_Sample

---
+++ Sample_Text_to_Voice_v1.0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 22:58:51 2023
+
+@author: user
+"""
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello There, This is my First voice, hope you like it.")
+engine.runAndWait()
